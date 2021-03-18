4ac96a5b351bb503b83e0b0f2fedcc4bd34aa7ff
garantony <garantony@mail.ru>
2021-03-18 19:15:07 +0400

    complete homework

--- homework18/task3.js
+++ homework18/task3.js
@@ -6,5 +6,6 @@ const fs = require('fs/promises');
 fs.readFile("./input.txt").then(function(val){
   const part1 = val.toString().substr(val.length[0], val.length/2);
   const part2 = val.toString().slice(val.length/2);
-  fs.rea
+  fs.writeFile("output1.txt", part1);
+  fs.writeFile("output2.txt", part2);
 })
