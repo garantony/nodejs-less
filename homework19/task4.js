2497c9535b9826a478df0611ad670f7a3eeaaae5
garantony <garantony@mail.ru>
2021-03-23 13:19:03 +0400

    complete homework19

--- homework19/task4.js
+++ homework19/task4.js
@@ -1,28 +1,25 @@
 // 4.Ստեղծել Promise վերադարձնող ֆունկցիա որը ստանում է տեքստ ֆայլի հասցե և տեքստի բառերի առաջին տառերը սաքում մեծատառ․
-const fs = require('fs').promises;
-// function strFirstCharToUppercase(path) {
-//   return new Promise(function(resolve, reject) {
-//     let read = fs.readFile(path).toString();
-//     fs.writeFile(path, read.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' '));
-//     resolve();
-//   });
-// }
-//
-// strFirstCharToUppercase('./text.txt').then(function () {
-//   console.log("date")
-// }).catch(function (err) {
-//   console.log(err)
-// })
+const fs = require('fs');
 
-
-async function firstLetterToUppercase(path) {
-    const data = (await fs.readFile(path)).toString();
-    // data.replace(/^(\w)|\s+(\w)/gi, letter => letter.toUpperCase())
-    await fs.writeFile(path, data.split(/\b/).map(word => word[0].toUpperCase() + word.slice(1)).join(''));
+function strFirstCharToUppercase() {
+  return new Promise(function(resolve, reject) {
+    fs.readFile('./text.txt', (err, date) => {
+      if (err) {
+        throw err;
+        reject()
+      } else {
+        fs.writeFile('./text.txt', date.toString().split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' '), (err, date) => {
+          if (err) {
+            throw err;
+          }
+        });
+        resolve();
+      }
+    });
+  });
 }
-
-firstLetterToUppercase('./text.txt').then(() => {
-    console.log('done');
-}).catch((err) => {
-    console.log(err);
+strFirstCharToUppercase().then(function () {
+  console.log("done")
+}).catch(function (err) {
+  console.log(err)
 })
