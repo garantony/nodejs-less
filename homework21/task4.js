392bbba1aa03845121c47627b7b0e88258fe45d0
garantony <garantony@mail.ru>
2021-03-25 18:46:50 +0400

    completed homework21

--- homework21/task4.js
+++ homework21/task4.js
@@ -1,1 +1,7 @@
-// 4․ Ստեղծել myMap մեթոդ Array-ի համարար , որը կաշխատի ինչպես map մեթոդը. [].map(fn) -ը կարողանանք գրել [].myMap(fn) -ի միջոցով ։
+// 4․ Ստեղծել myMap մեթոդ Array-ի համարար , որը կաշխատի ինչպես map մեթոդը. [].map(fn) -ը կ
+// արողանանք գրել [].myMap(fn) -ի միջոցով ։
+
+Array.prototype.myMap = function(){
+    this.map(i=>console.log(i))
+};
+[1,2,3].myMap();
